ceb893035c138bef6ae52e339f57e76ccb9bc8d4
Mark Whitby <github@chipfat.com>
2019-12-10 16:34:21 +0000

    Restructure repo, separate out code, pipelines and charts

--- server/deploy/deploy-staging.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-productionSlot=$(helm get values cyan-api-server | awk '/productionSlot/{print $2}')
-if [ $productionSlot = "blue" ]; then
-    stagingSlot="green"
-else
-    stagingSlot="blue"
-fi
-helm upgrade cyan-api-server server --set $stagingSlot.enabled=true,$stagingSlot.tag=175 --reuse-values
