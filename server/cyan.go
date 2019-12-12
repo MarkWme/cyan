56be64e158a05f793c2866049ab7df0ba4bca5bc
Mark Whitby <github@chipfat.com>
2019-12-12 00:48:03 +0000

    Implementing preStop delay for SIGTERM

--- server/cyan.go
+++ server/cyan.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 type version struct {
@@ -18,8 +19,16 @@ func getVersion(w http.ResponseWriter, r *http.Request) {
 	log.Println(getVersionValue())
 }
 
+func podTerminate(w http.ResponseWriter, r *http.Request) {
+	log.Println("podTerminate endpoint")
+	log.Println("Waiting 10 seconds ...")
+	time.Sleep(10 * time.Second)
+	log.Println("Done")
+}
+
 func main() {
 	http.HandleFunc("/api/getVersion", getVersion)
+	http.HandleFunc("/api/podTerminate", podTerminate)
 	log.Println("Starting HTTP server on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
