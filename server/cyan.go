b8e14541d106094a1802acc78acd65613e685c1d
Mark Whitby <github@chipfat.com>
2019-12-16 15:13:13 +0000

    Add readiness probe support

--- server/cyan.go
+++ server/cyan.go
@@ -12,6 +12,8 @@ type version struct {
 	Version     string
 }
 
+var ready bool = true
+
 func getVersion(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(getVersionValue())
@@ -20,13 +22,26 @@ func getVersion(w http.ResponseWriter, r *http.Request) {
 
 func podTerminate(w http.ResponseWriter, r *http.Request) {
 	log.Println("podTerminate endpoint: Starting 30 second waiting period ...")
+	ready = false
 	time.Sleep(30 * time.Second)
 	log.Println("Waiting period complete")
+}
+
+func podReady(w http.ResponseWriter, r *http.Request) {
+	log.Println("podReady endpoint:", ready)
+	if ready {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 }
 
 func main() {
 	http.HandleFunc("/api/getVersion", getVersion)
 	http.HandleFunc("/api/podTerminate", podTerminate)
+	http.HandleFunc("/api/podReady", podReady)
 	log.Println("Starting HTTP server on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
